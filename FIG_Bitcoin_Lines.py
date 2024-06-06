0e114f80c4d3e2777b92d80ef3db2d76ee48626c
Guillem <63495701+guillemmaya92@users.noreply.github.com>
2024-06-06 19:39:55 +0200

    Add files via upload

--- FIG_Bitcoin_Lines.py
+++ FIG_Bitcoin_Lines.py
@@ -4,6 +4,7 @@ import pandas as pd
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
 import plotnine as p9
 import requests
 
@@ -19,12 +20,81 @@ dataframes = []
 # Iterate API with each date
 for to_ts in dp['to_ts']:
     # Build an URL with parameters and transform data
-    url = f"https://min-api.cryptocompare.com/data/v2/histoday?fsym=ETH&tsym=USD&limit=365&toTs={to_ts}"
+    url = f"https://min-api.cryptocompare.com/data/v2/histoday?fsym=BTC&tsym=USD&limit=365&toTs={to_ts}"
     response = requests.get(url)
     data = response.json().get("Data", {}).get("Data", [])
     df = pd.DataFrame([
         {
-            "symbol": "ETHUSD",
+            "symbol": "BTCUSD",
+            "date": pd.to_datetime(entry["time"], unit="s").date(),
+            "open": entry["open"],
+            "close": entry["close"],
+            "low": entry["low"],
+            "high": entry["high"],
+            "volume": entry["volumeto"]
+        }
+        for entry in data
+    ])
+    dataframes.append(df)
+# Combine all df into one
+btc = pd.concat(dataframes, ignore_index=True)
+
+# DataSet 0 - Halving
+#================================================================================
+halving = {'halving': [0 , 1, 2, 3, 4],
+           'date': ['2009-01-03', '2012-11-28', '2016-07-09', '2020-05-11', '2024-04-20'] 
+           }
+
+halving = pd.DataFrame(halving)
+halving['date'] = pd.to_datetime(halving['date'])
+
+# DataSet 1 - BTC Price
+# ==============================================================================
+# Definir y ordenar dataset
+btc = btc.drop_duplicates()
+btc['date'] = pd.to_datetime(btc['date'])
+btc = btc.set_index('date')
+btc = btc.asfreq('D').ffill()
+btc = btc.reset_index()
+btc.sort_values(by=['date'], inplace=True)
+btc = pd.merge(btc, halving, on='date', how='left')
+btc['halving'].fillna(method='ffill', inplace=True)
+btc['halving'].fillna(0, inplace=True)
+btc['halving'] = btc['halving'].astype(int)
+btc['first_close'] = btc.groupby('halving')['close'].transform('first')
+btc['increase'] = (btc['close'] - btc['first_close']) / btc['first_close'] * 100
+btc['days'] = btc.groupby('halving').cumcount() + 1
+btc['daystotal'] = btc.groupby('date').cumcount() + 1
+btc['closelog'] = np.log10(btc['close'])
+btc = btc[btc['halving'] >= 1]
+
+# Libraries
+# ==============================================================================
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+import plotnine as p9
+import requests
+
+# Get API Data
+# ==============================================================================
+# Create a df with final year dates
+dp = pd.DataFrame({'date': pd.date_range(start='2010-12-31', end='2024-12-31', freq='Y')})
+dp['to_ts'] = dp['date'].apply(lambda x: int(pd.to_datetime(x).timestamp()))
+
+# Create an empty list
+dataframes = []
+
+# Iterate API with each date
+for to_ts in dp['to_ts']:
+    # Build an URL with parameters and transform data
+    url = f"https://min-api.cryptocompare.com/data/v2/histoday?fsym=BTC&tsym=USD&limit=365&toTs={to_ts}"
+    response = requests.get(url)
+    data = response.json().get("Data", {}).get("Data", [])
+    df = pd.DataFrame([
+        {
+            "symbol": "BTCUSD",
             "date": pd.to_datetime(entry["time"], unit="s").date(),
             "open": entry["open"],
             "close": entry["close"],
@@ -66,6 +136,80 @@ btc['days'] = btc.groupby('halving').cumcount() + 1
 btc['closelog'] = np.log10(btc['close'])
 btc = btc[btc['halving'] >= 1]
 
+# Graph 1 - PLOTNINE
+# ==============================================================================
+# Plot a graph
+# Calculate maximum closelog and days for each halving
+max_vals = btc.groupby('halving').agg({'closelog': 'last', 'days': 'max'}).reset_index()
+
+# Dictionary to hold halving labels
+halving_labels = {
+    0: 'Introduction',
+    1: '1st Halving',
+    2: '2nd Halving',
+    3: '3rd Halving',
+    4: '4th Halving'
+}
+
+plot = (
+    p9.ggplot(btc, p9.aes(x='days', y='closelog', color='factor(halving)'))
+    + p9.geom_rect(
+        p9.aes(xmin=0, xmax=500, ymin=-float('inf'), ymax=float('inf')),
+        fill='#F5FFFA',
+        alpha=0.1,
+        inherit_aes=False
+    )
+    + p9.geom_rect(
+        p9.aes(xmin=500, xmax=1000, ymin=-float('inf'), ymax=float('inf')),
+        fill='#FFF2F2',
+        alpha=0.1,
+        inherit_aes=False
+    )
+    + p9.geom_rect(
+        p9.aes(xmin=1000, xmax=1500, ymin=-float('inf'), ymax=float('inf')),
+        fill='#FEFDF9',
+        alpha=0.1,
+        inherit_aes=False
+    )
+    + p9.geom_line(size=0.5)
+    + p9.labs(
+        title='BTC Log Price - From Each Halving',
+        x='Days',
+        y='Log Price',
+        color='Halving'
+    )
+    + p9.xlim(0, 1500)
+    + p9.theme_minimal()
+    + p9.scale_color_manual(
+        name='Halving',
+        values = {
+            0: '#E0E0E0',  # Very Light Grey
+            1: '#C0C0C0',  # Light Grey
+            2: '#808080',  # Medium Grey
+            3: '#404040',  # Dark Grey
+            4: '#8B0000'   # Red
+        },
+        labels = {
+            0: 'Int: 2009-01-03 to 2012-11-28', # Introdution
+            1: '1st: 2012-11-28 to 2016-07-09', # 1st Halving
+            2: '2nd: 2016-07-09 to 2020-05-11', # 2nd Halving
+            3: '3rd: 2020-05-11 to 2024-04-20', # 3rd Halving
+            4: '4th: 2024-04-20 to present' # 4th Halving
+        }
+    )
+    + [p9.annotate('text', x=row['days']+60, y=row['closelog'], label=halving_labels[row['halving']], ha='center', va='top', size=8) for idx, row in max_vals.iterrows()]
+    + p9.theme(
+        figure_size=(16, 9),
+        axis_text_x=p9.element_text(rotation=45, hjust=1),
+        text=p9.element_text(family="Open Sans"),
+        plot_title=p9.element_text(size=14, weight='bold'),
+        panel_background=p9.element_rect(fill="white", color="white")
+    )
+)
+
+# Print it!
+print(plot)
+
 # Graph 2 - SEABORN
 # ==============================================================================
 # Font Style
@@ -73,10 +217,10 @@ plt.rcParams.update({'font.family': 'sans-serif', 'font.sans-serif': ['Open Sans
 
 # Colors Background
 regions = [
-    (0, 500, '#6B8E23'),  # Green
-    (500, 1000, '#FF4500'),  # Red
-    (1000, 1500, '#FFA500')
-    ]  # Orange
+    (0, 500, '#6B8E23'),   # Green
+    (500, 1000, '#FF4500'), # Red
+    (1000, 1500, '#FFA500') # Orange
+]
 
 # Colors Palette Lines
 lines = {
@@ -89,68 +233,77 @@ lines = {
 
 # Seaborn to plot a graph
 sns.set(style="whitegrid", rc={"grid.color": "0.95", "axes.grid.axis": "y"})
-plt.figure(figsize=(16, 9))
-sns.lineplot(x='days', y='closelog', hue='halving', data=btc, markers=True, palette=lines, linewidth=1)
+fig, ax = plt.subplots(figsize=(16, 9))
 
-# Add region colors in the background
-for start, end, color in regions:
-    plt.axvspan(start, end, color=color, alpha=0.05)
+# Function to update the plot for each frame
+def update(frame):
+    ax.clear()
+    df_filtered = btc[btc['daystotal'] <= frame]
+    sns.lineplot(x='days', y='closelog', hue='halving', data=df_filtered, markers=True, palette=lines, linewidth=1, ax=ax)
+    
+    # Add region colors in the background
+    for start, end, color in regions:
+        ax.axvspan(start, end, color=color, alpha=0.05)
 
-# Title and axis
-plt.title('BTC Log Price - From Each Halving', fontsize=16, fontweight='bold', pad=20)
-plt.xlabel('Days',  fontsize=10)
-plt.ylabel('Log Price', fontsize=10)
-plt.xlim(0, 1500)
-plt.xticks(range(0, 1501, 125), fontsize=9)
-plt.yticks(fontsize=9)
+    # Title and axis
+    ax.set_title('BTC Log Price - From Each Halving', fontsize=16, fontweight='bold', pad=20)
+    ax.set_xlabel('Days', fontsize=10)
+    ax.set_ylabel('Log Price', fontsize=10)
+    ax.set_xlim(0, 1500)
+    ax.set_ylim(8, 12)  # Adjust the y-axis limits as needed
+    ax.set_xticks(range(0, 1501, 125))
+    ax.set_yticks(np.arange(8, 12.1, 0.5))
 
-# Custom legend
-legend = plt.legend(title="Halving", loc='lower right', fontsize=8, title_fontsize='10')
-new_title = 'Dates:'
-legend.set_title(new_title)
-new_labels = ['1st Halving: 2012-11-28 to 2016-07-09', '2nd Halving: 2016-07-09 to 2020-05-11', '3rd Halving: 2020-05-11 to 2024-04-20', '4th Halving: 2024-04-20 to present'] # Adjust the number of labels according to your data
-for text, new_label in zip(legend.texts, new_labels):
-    text.set_text(new_label)
+    # Custom legend
+    legend = ax.legend(title="Halving", loc='lower right', fontsize=8, title_fontsize='10')
+    new_title = 'Dates:'
+    legend.set_title(new_title)
+    new_labels = ['1st Halving: 2012-11-28 to 2016-07-09', '2nd Halving: 2016-07-09 to 2020-05-11', '3rd Halving: 2020-05-11 to 2024-04-20', '4th Halving: 2024-04-20 to present'] # Adjust the number of labels according to your data
+    for text, new_label in zip(legend.texts, new_labels):
+        text.set_text(new_label)
 
-# Maximo First 750 days
-btc1 = btc[(btc['days'] >= 0) & (btc['days'] <= 750)]
-for halving, group in btc1.groupby('halving'):
-    max_value = group['closelog'].max()
-    max_row = group[group['closelog'] == max_value].iloc[0]
-    plt.plot(max_row['days'], max_row['closelog'] +0.05, marker='*', color='darkgoldenrod', markersize=5)
-    plt.text(max_row['days'], max_row['closelog'] +0.1, f'{max_row["close"]:,.0f} ({max_row["increase"]:,.0f})%', fontsize=7, ha='center', color='darkgoldenrod')
+    # Maximo First 750 days
+    btc1 = btc[(btc['days'] >= 0) & (btc['days'] <= 750)]
+    for halving, group in btc1.groupby('halving'):
+        max_value = group['closelog'].max()
+        max_row = group[group['closelog'] == max_value].iloc[0]
+        ax.plot(max_row['days'], max_row['closelog'] +0.05, marker='*', color='darkgoldenrod', markersize=5)
+        ax.text(max_row['days'], max_row['closelog'] +0.1, f'{max_row["close"]:,.0f} ({max_row["increase"]:,.0f})%', fontsize=7, ha='center', color='darkgoldenrod')
 
-# Min Between 500 and 1000 days
-btc2 = btc[(btc['days'] >= 500) & (btc['days'] <= 1000)]
-for halving, group in btc2.groupby('halving'):
-    min_value = group['closelog'].min()
-    min_row = group[group['closelog'] == min_value].iloc[0]
-    plt.plot(min_row['days'], min_row['closelog'] - 0.05, marker='v', color='darkred', markersize=5)
-    plt.text(min_row['days'], min_row['closelog'] -0.15, f'{min_row["close"]:,.0f} ({min_row["increase"]:,.0f})%', fontsize=7, ha='center', color='darkred')
+    # Min Between 500 and 1000 days
+    btc2 = btc[(btc['days'] >= 500) & (btc['days'] <= 1000)]
+    for halving, group in btc2.groupby('halving'):
+        min_value = group['closelog'].min()
+        min_row = group[group['closelog'] == min_value].iloc[0]
+        ax.plot(min_row['days'], min_row['closelog'] - 0.05, marker='v', color='darkred', markersize=5)
+        ax.text(min_row['days'], min_row['closelog'] -0.15, f'{min_row["close"]:,.0f} ({min_row["increase"]:,.0f})%', fontsize=7, ha='center', color='darkred')
 
-# Max After 750 days 
-btc3 = btc[(btc['days'] >= 750) & (btc['days'] <= 1500)]
-for halving, group in btc3.groupby('halving'):
-    max_value = group['closelog'].max()
-    max_row = group[group['closelog'] == max_value].iloc[0]
-    plt.plot(max_row['days'], max_row['closelog'] +0.05, marker='^', color='darkgreen', markersize=5)
-    plt.text(max_row['days'], max_row['closelog'] +0.1, f'{max_row["close"]:,.0f} ({max_row["increase"]:,.0f})%', fontsize=7, ha='center', color='darkgreen')
+    # Max After 750 days 
+    btc3 = btc[(btc['days'] >= 750) & (btc['days'] <= 1500)]
+    for halving, group in btc3.groupby('halving'):
+        max_value = group['closelog'].max()
+        max_row = group[group['closelog'] == max_value].iloc[0]
+        ax.plot(max_row['days'], max_row['closelog'] +0.05, marker='^', color='darkgreen', markersize=5)
+        ax.text(max_row['days'], max_row['closelog'] +0.1, f'{max_row["close"]:,.0f} ({max_row["increase"]:,.0f})%', fontsize=7, ha='center', color='darkgreen')
 
-# Custom Last Dots
-max_vals = btc.groupby('halving').agg({'closelog': 'last', 'days': 'max'}).reset_index()
-for index, row in max_vals.iterrows():
-    plt.plot(row['days'], row['closelog'], 'ro', markersize=2)
+    # Custom Last Dots
+    max_vals = btc.groupby('halving').agg({'closelog': 'last', 'days': 'max'}).reset_index()
+    for index, row in max_vals.iterrows():
+        ax.plot(row['days'], row['closelog'], 'ro', markersize=2)
 
-# Custom Line labels
-for halving, group in btc.groupby('halving'):
-    last_point = group.iloc[-1]
-    x = last_point['days']
-    y = last_point['closelog']
-    max_days = group['days'].max()
-    plt.text(x + 8, y, f'Halving {halving}\n{max_days} days', color=lines[halving], fontsize=8, ha='left', va='center')
+    # Custom Line labels
+    for halving, group in btc.groupby('halving'):
+        last_point = group.iloc[-1]
+        x = last_point['days']
+        y = last_point['closelog']
+        max_days = group['days'].max()
+        ax.text(x + 8, y, f'Halving {halving}\n{max_days} days', color=lines[halving], fontsize=8, ha='left', va='center')
 
-# Adjust layout
-plt.tight_layout()
+    # Adjust layout
+    plt.tight_layout()
 
-# Print it!
+# Crear la animación
+ani = FuncAnimation(fig, update, frames=btc['daystotal'].unique(), interval=0.1)
+
+# Mostrar la animación
 plt.show()
