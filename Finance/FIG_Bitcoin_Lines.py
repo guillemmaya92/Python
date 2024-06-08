8cd9898963c8976008ffb1a059a8c419687d1b22
Guillem <63495701+guillemmaya92@users.noreply.github.com>
2024-06-08 17:13:19 +0200

    Update FIG_Bitcoin_Lines.py

--- Finance/FIG_Bitcoin_Lines.py
+++ Finance/FIG_Bitcoin_Lines.py
@@ -52,6 +52,7 @@ halving['date'] = pd.to_datetime(halving['date'])
 # Definir y ordenar dataset
 btc = btc.drop_duplicates()
 btc['date'] = pd.to_datetime(btc['date'])
+btc['year_month'] = btc['date'].dt.strftime('%Y-%m')
 btc = btc.set_index('date')
 btc = btc.asfreq('D').ffill()
 btc = btc.reset_index()
@@ -63,9 +64,9 @@ btc['halving'] = btc['halving'].astype(int)
 btc['first_close'] = btc.groupby('halving')['close'].transform('first')
 btc['increase'] = (btc['close'] - btc['first_close']) / btc['first_close'] * 100
 btc['days'] = btc.groupby('halving').cumcount() + 1
-btc['daystotal'] = btc.groupby('symbol').cumcount() + 1
 btc['closelog'] = np.log10(btc['close'])
 btc = btc[btc['halving'] >= 1]
+btc['daystotal'] = btc.groupby('symbol').cumcount() + 1
 
 # Graph 1 - SEABORN
 # ==============================================================================
@@ -98,11 +99,12 @@ for start, end, color in regions:
     plt.axvspan(start, end, color=color, alpha=0.05)
 
 # Title and axis
-plt.title('BTC Log Price - From Each Halving', fontsize=16, fontweight='bold', pad=20)
-plt.xlabel('Days',  fontsize=10)
-plt.ylabel('Log Price', fontsize=10)
+plt.title('Bitcoin Logarithmic Trends: Analysis Since Each Halving', fontsize=16, fontweight='bold', pad=20)
+plt.xlabel('Days',  fontsize=10, fontweight='bold')
+plt.ylabel('Log Price', fontsize=10, fontweight='bold')
 plt.xlim(0, 1500)
 plt.xticks(range(0, 1501, 125), fontsize=9)
+plt.tick_params(axis='both', labelsize=8)
 plt.yticks(fontsize=9)
 
 # Custom legend
@@ -149,6 +151,16 @@ for halving, group in btc.groupby('halving'):
     y = last_point['closelog']
     max_days = group['days'].max()
     plt.text(x + 8, y, f'Halving {halving}\n{max_days} days', color=lines[halving], fontsize=8, ha='left', va='center')
+    
+# Add Year Label
+current_year_month = btc['year_month'].max() 
+plt.text(1, 1.05, f'{current_year_month}',
+    transform=plt.gca().transAxes,
+    fontsize=22, ha='right', va='top',
+    fontweight='bold', color='#D3D3D3')
+
+# Add Data Source
+plt.text(0, -0.065, 'Data Source: CryptoCompare', transform=plt.gca().transAxes, fontsize=8, color='gray')
 
 # Adjust layout
 plt.tight_layout()
