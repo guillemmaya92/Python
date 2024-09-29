7e17bbc1479026a63f0040d3acecfdeb61bfd4d4
Guillem <63495701+guillemmaya92@users.noreply.github.com>
2024-09-29 21:07:17 +0200

    Update FIG_PPP_Inequalities_animation.py

--- Finance/FIG_PPP_Inequalities_animation.py
+++ Finance/FIG_PPP_Inequalities_animation.py
@@ -8,6 +8,7 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import matplotlib.ticker as ticker
+from datetime import datetime
 
 # Data Extraction (Countries)
 # =====================================================================
@@ -22,7 +23,7 @@ df_countries = df.rename(columns={'index': 'ISO3'})
 # Data Extraction - IMF (1980-2030)
 # =====================================================================
 #Parametro
-parameters = ['PPPPC', 'NGDPDPC', 'LP']
+parameters = ['NGDPD', 'PPPGDP', 'LP']
 
 # Create an empty list
 records = []
@@ -52,22 +53,48 @@ df_imf = pd.DataFrame(records)
 df_imf = df_imf.pivot(index=['ISO3', 'Year'], columns='Parameter', values='Value').reset_index()
 
 # Filter after 2024
-df_imf = df_imf[df_imf['Year'] >= 1999]
+df_imf = df_imf[df_imf['Year'] >= 1980]
 
 # Data Manipulation
 # =====================================================================
 # Concat and filter dataframes
-df = df_imf.dropna(subset=['NGDPDPC', 'PPPPC', 'LP'], how='any')
+df = df_imf.dropna(subset=['NGDPD', 'PPPGDP', 'LP'], how='any')
+
+# Create a list
+dfs = []
+
+# Interpolate daily data
+for iso3 in df['ISO3'].unique():
+    temp_df = df[df['ISO3'] == iso3].copy()
+    temp_df['Date'] = pd.to_datetime(temp_df['Year'], format='%Y')
+    temp_df = temp_df[['Date', 'NGDPD', 'PPPGDP', 'LP']]
+    temp_df = temp_df.set_index('Date').resample('ME').mean().interpolate(method='linear').reset_index()
+    temp_df['ISO3'] = iso3
+    temp_df['Year'] = temp_df['Date'].dt.year 
+    dfs.append(temp_df)
+
+# Concat dataframes    
+df = pd.concat(dfs, ignore_index=True)
 
 # Merge queries
 df = df.merge(df_countries, how='left', left_on='ISO3', right_on='ISO3')
-df = df[['Region', 'ISO3', 'Country', 'Cod_Currency', 'Year', 'NGDPDPC', 'PPPPC', 'LP']]
+df = df[['Region', 'ISO3', 'Country', 'Cod_Currency', 'Year', 'Date', 'NGDPD', 'PPPGDP', 'LP']]
 df = df[df['Cod_Currency'].notna()]
 
 # Calculate PPP
-df = df.groupby(['Region', 'ISO3', 'Year'])[['NGDPDPC', 'PPPPC', 'LP']].sum()
+df = df.groupby(['Region', 'ISO3', 'Year', 'Date'])[['NGDPD', 'PPPGDP', 'LP']].sum()
 df = df.reset_index()
-df['PPP'] = df['NGDPDPC'] / df['PPPPC']
+df['PPP'] = df['NGDPD'] / df['PPPGDP']
+df['NGDPDPC'] = df['NGDPD'] / df['LP']
+df['PPPPC'] = df['PPPGDP'] / df['LP']
+
+# Calculate Average Weight and Percent
+df['AVG_Weight'] = df.groupby('Date')['NGDPDPC'].transform(lambda x: np.average(x, weights=df.loc[x.index, 'LP']))
+df['Percent'] = df['NGDPD'] / df.groupby('Date')['NGDPD'].transform('sum')
+
+# Filtering
+df = df[df['NGDPDPC'] < df['AVG_Weight'] * 60 ]
+df = df[df['PPP'] < 1.2]
 
 print(df)
 
@@ -98,14 +125,14 @@ df['custom_line'] = df['Region'].map(custom_line)
 
 fig, ax = plt.subplots(figsize=(10, 10))
 
-def update(year):
+def update(Date):
     # Filtrar datos por año
     ax.clear()
-    filtered_df = df[df['Year'] == year]
-    usa = filtered_df.loc[filtered_df['ISO3'] == 'USA', 'NGDPDPC'].max() * 1.1
+    filtered_df = df[df['Date'] == Date]
+    usa = filtered_df['AVG_Weight'].max() * 10
 
     # Crear gráfico de dispersión
-    scatter = ax.scatter(filtered_df['PPP'], filtered_df['NGDPDPC'], s=filtered_df['LP']/2,
+    scatter = ax.scatter(filtered_df['PPP'], filtered_df['NGDPDPC'], s=filtered_df['Percent'] * 10000,
                          edgecolor=filtered_df['custom_line'], facecolor=filtered_df['custom_area'],
                          linewidth=0.5)
 
@@ -116,25 +143,38 @@ def update(year):
     ax.set_ylabel('GDP Per Capita ($US)', fontsize=10, fontweight='bold')
     ax.set_xlim(0, 1.2)
     ax.set_ylim(0, usa)
-    ax.yaxis.set_major_formatter(ticker.FuncFormatter(lambda x, _: f'{int(x):,}'))
+    ax.yaxis.set_major_formatter(ticker.FuncFormatter(lambda x, _: f'{int(x):,}k'))
     ax.grid(True, linestyle='-', color='grey', linewidth=0.08)
     ax.set_yticks(np.linspace(0, usa, 7))
 
     # Añadir línea de tendencia
-    z = np.polyfit(filtered_df['PPP'], filtered_df['NGDPDPC'], 1)
+    z = np.polyfit(filtered_df['PPP'], filtered_df['NGDPDPC'], 1, w=filtered_df['NGDPD'])
     p = np.poly1d(z)
     ax.plot(filtered_df['PPP'], p(filtered_df['PPP']), color='darkred', linewidth=0.5)
 
     # Rellenar áreas roja y verde
     ax.fill_betweenx(y=[0, usa], x1=0, x2=1, color='red', alpha=0.04)
     ax.fill_betweenx(y=[0, usa], x1=1, x2=1.25, color='green', alpha=0.04)
+    
+    # Add Year label
+    formatted_date = Date.strftime('%Y-%m')
+    ax.text(0.95, 1.06, f'{formatted_date}',
+             transform=plt.gca().transAxes,
+             fontsize=22, ha='right', va='top',
+             fontweight='bold', color='#D3D3D3')
+    
+    # Add Data Source
+    ax.text(0, -0.1, 'Data Source: IMF World Economic Outlook Database, 2024', 
+    transform=plt.gca().transAxes, 
+    fontsize=8, 
+    color='gray')
 
 # Configurar animación
-years = sorted(df['Year'].unique())
+years = sorted(df['Date'].unique())
 ani = animation.FuncAnimation(fig, update, frames=years, repeat=False, interval=250, blit=False)
 
 # Guardar la animación :)
-ani.save('C:/Users/guillem.maya/Desktop/FIG_PPP_Inequalities.webp', writer='imagemagick', fps=2)
+ani.save('C:/Users/guill/Desktop/FIG_PPP_Inequalities2.webp', writer='imagemagick', fps=25)
 
 # Mostrar el gráfico
 plt.show()
