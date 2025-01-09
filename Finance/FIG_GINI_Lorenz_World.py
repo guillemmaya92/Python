bdc522a88f50eda735b0add394f9a41f30e23e01
Guillem <63495701+guillemmaya92@users.noreply.github.com>
2025-01-09 21:31:59 +0100

    Update FIG_GINI_Lorenz_World.py

--- Finance/FIG_GINI_Lorenz_World.py
+++ Finance/FIG_GINI_Lorenz_World.py
@@ -1,15 +1,18 @@
 # Libraries
 # ===================================================
 import requests
-import os
 import pandas as pd
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
-from matplotlib.ticker import FuncFormatter
-import matplotlib.patches as patches
 import matplotlib.lines as mlines
 import matplotlib.patheffects as patheffects
+
+# Parameters
+# =====================================================================
+# Select between Income/Wealth
+selection = 'Income'
+year = 2021
 
 # Data Extraction (Countries)
 # =====================================================================
@@ -21,7 +24,7 @@ df = pd.DataFrame(data)
 df = pd.DataFrame.from_dict(data, orient='index').reset_index()
 df_countries = df.rename(columns={'index': 'ISO3'})
 
-# Data Extraction
+# Data Extraction (Percentages)
 # ===================================================
 # URL GitHub
 url = "https://raw.githubusercontent.com/guillemmaya92/Python/main/Data/WID_Percentiles.parquet"
@@ -30,20 +33,43 @@ url = "https://raw.githubusercontent.com/guillemmaya92/Python/main/Data/WID_Perc
 df = pd.read_parquet(url, engine='pyarrow')
 
 # Filter year
-df = df[df['year'] == 2021]
+df = df[df['year'] == year]
+
+# Data Extraction (Values)
+# ===================================================
+# URL GitHub
+url = "https://raw.githubusercontent.com/guillemmaya92/Python/main/Data/WID_Values.parquet"
+
+# Extract data from parquet
+dfv = pd.read_parquet(url, engine='pyarrow')
+
+# Filter year
+dfv = dfv[dfv['year'] == year]
+
+# Extract world values
+gincomew = dfv.loc[dfv['country'] == 'WO', 'gincome'].iloc[0]
+gwealthw = dfv.loc[dfv['country'] == 'WO', 'gwealth'].iloc[0]
+
+# Extract countries weighted average values
+dfincome = dfv[dfv['country'].isin(df_countries['ISO2']) & dfv['gincome'].notnull() & dfv['population'].notnull()]
+dfwealth = dfv[dfv['country'].isin(df_countries['ISO2']) & dfv['gwealth'].notnull() & dfv['population'].notnull()]
+
+gincomec = np.average(dfincome['gincome'], weights= dfincome['population'])
+gwealthc = np.average(dfwealth['gwealth'], weights= dfwealth['population'])
+
+# Dynamic value
+giniw = round(gwealthw, 2) if selection == 'Wealth' else round(gincomew, 2)
+ginic= round(gwealthc, 2) if selection == 'Wealth' else round(gincomec, 2)
 
 # Data Manipulation
 # ===================================================
-# Select between income and wealth
-selection = 'Wealth'
-
 # Calculate cummulative
 df['percentile'] =  df['percentile'] / 100
 df['income'] =  df['income'] / 100
 df['wealth'] =  df['wealth'] / 100
 df['income_cum'] =  df.groupby(['country'])['income'].cumsum() / df.groupby(['country'])['income'].transform('sum')
 df['wealth_cum'] =  df.groupby(['country'])['wealth'].cumsum() / df.groupby(['country'])['wealth'].transform('sum')
-df['value_cum'] = df['income_cum'] if selection == 'income' else df['wealth_cum']
+df['value_cum'] = df['income_cum'] if selection == 'Income' else df['wealth_cum']
 
 # Countries
 dfc = df.merge(df_countries, how='left', left_on='country', right_on='ISO2')
@@ -101,7 +127,7 @@ plt.plot([0, 1], [0, 1], color="gray", linestyle="-", linewidth=1)
 
 # Configuración del gráfico
 plt.text(0, 1.05, f'Global {selection} Distribution', fontsize=13, fontweight='bold', ha='left', transform=plt.gca().transAxes)
-plt.text(0, 1.02, 'Lorenz Curve Comparision Across Countries', fontsize=9, color='#262626', ha='left', transform=plt.gca().transAxes)
+plt.text(0, 1.02, 'A global and national perspective on Lorenz curves', fontsize=9, color='#262626', ha='left', transform=plt.gca().transAxes)
 plt.xlabel('Cumulative Population (%)', fontsize=10, fontweight='bold')
 plt.ylabel(f'Cumulative {selection} (%)', fontsize=10, fontweight='bold')
 plt.xlim(0, 1)
@@ -138,15 +164,11 @@ plt.text(0.85, -0.1, '@guillemmaya.com',
     color='#212121')
 
  # Add Year label
-formatted_date = 2022
+formatted_date = year
 plt.text(1, 1.06, f'{formatted_date}',
     transform=plt.gca().transAxes,
     fontsize=22, ha='right', va='top',
     fontweight='bold', color='#D3D3D3')
-
-# Gini Values
-ginic = 0.64
-giniw = 0.75
 
 # Create custom lines
 intra_line = mlines.Line2D([], [], color='#808080', label=f'Gini Intra-Countries: {ginic}', linewidth=2)
@@ -158,8 +180,7 @@ inter_circle = mlines.Line2D([], [], marker='o', color='w', markerfacecolor='#FF
 plt.legend(handles=[intra_line, inter_line])
 
 # Save the figure
-plt.savefig('C:/Users/guillem.maya/Desktop/FIG_WID_Global_Lorenz_Curves.png', format='png', dpi=300, bbox_inches='tight')
+plt.savefig('C:/Users/guill/Desktop/FIG_WID_Global_Lorenz_Curves.png', format='png', dpi=300, bbox_inches='tight')
 
 # Show the plot!
 plt.show()
-
